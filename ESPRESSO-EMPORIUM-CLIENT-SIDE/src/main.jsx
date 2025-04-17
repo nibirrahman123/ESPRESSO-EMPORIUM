7ce936049c89c2aa4a207b71757163debb4a51ac
nibirrahman123 <nibir3211@gmail.com>
2025-04-17 07:28:57 +0600

    added client side

---
+++ ESPRESSO-EMPORIUM-CLIENT-SIDE/src/main.jsx
@@ -0,0 +1,28 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Home from './Home.jsx';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './layout/Root.jsx';
+import AddCoffee from './components/AddCoffee.jsx';
+import UpdateCoffee from './components/UpdateCoffee.jsx';
+
+const router = createBrowserRouter([
+  {path: '/', element: <Root /> , children:[
+    {path:'/home',element: <Home />},
+    {path:'/addCoffee',element: <AddCoffee />},
+    {path:'/updateCoffee',element: <UpdateCoffee />}
+  ]}
+])
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
